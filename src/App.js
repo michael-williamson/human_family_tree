bc5ef392c852473ffc985a1364d82bcf1b98a517
michael-williamson <michaeldavid1721@yahoo.com>
2021-09-16 07:38:33 -0500

    adjusted Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { HumanGallery } from "./components/HumanGallery";
-import { MapPage } from "./components/MapPage";
+import { MapPage } from "./components/mapComponents/MapPage";
 import Button from "@material-ui/core/Button";
 
 function App() {
@@ -11,23 +11,24 @@ function App() {
       <div className="header_container">
         <h1 className="header_title">Human Family Tree</h1>
         <div className="header_overlay"></div>
+        <div className="button_container">
+          <Button
+            onClick={() => setShowMap(false)}
+            variant="contained"
+            color="primary"
+          >
+            Show Fossils
+          </Button>
+          <Button
+            onClick={() => setShowMap(true)}
+            variant="contained"
+            color="primary"
+          >
+            Show Map
+          </Button>
+        </div>
       </div>
-      <div className="button_container">
-        <Button
-          onClick={() => setShowMap(false)}
-          variant="contained"
-          color="primary"
-        >
-          Show Fossils
-        </Button>
-        <Button
-          onClick={() => setShowMap(true)}
-          variant="contained"
-          color="primary"
-        >
-          Show Map
-        </Button>
-      </div>
+
       {!showMap ? <HumanGallery /> : <MapPage />}
     </div>
   );
