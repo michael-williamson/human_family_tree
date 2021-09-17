7cfc8d07c009267996db5fada23538dd5d753f08
michael-williamson <michaeldavid1721@yahoo.com>
2021-09-17 11:23:12 -0500

    added italics to species font style

--- src/components/reusableComponents/CheckboxComp.js
+++ src/components/reusableComponents/CheckboxComp.js
@@ -13,6 +13,7 @@ const CheckBoxMapper = (props) => {
   const useStyles = makeStyles({
     root: {
       textAlign: "left",
+      fontStyle: "italic",
     },
   });
   const classes = useStyles();
