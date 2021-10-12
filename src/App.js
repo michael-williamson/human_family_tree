7b5dbeef235100ec7121b19ba192351f0ccfe012
michael-williamson <michaeldavid1721@yahoo.com>
2021-10-11 19:52:55 -0500

    New Navbar comp and header styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { Suspense, lazy } from "react";
 import "./App.css";
 
-import { Switch, Route } from "react-router";
-// import { FossilGalleryComponent } from "./components/fossilGalleryComponents/FossilGalleryComponent";
-// import { HumanGallery } from "./components/HumanGallery";
-// import { MapPage } from "./components/mapComponents/MapPage";
-import FullWidthTabs from "./components/reusableComponents/MuiTabsComp";
-import { Container } from "@material-ui/core";
-// import Button from "@material-ui/core/Button";
+import { Switch, Route, Redirect } from "react-router";
+import { Container, CircularProgress } from "@material-ui/core";
+import { Navbar } from "./components/Navbar";
 
 const MapPage = lazy(() => import("./components/mapComponents/MapPage.js"));
 const FossilGalleryComponent = lazy(() =>
@@ -24,17 +20,22 @@ function App() {
       >
         <div className="header_container">
           <h1 className="header_title">Human Family Tree</h1>
-          <FullWidthTabs />
+          <Navbar />
         </div>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<CircularProgress />}>
           <Switch>
             <Route exact path="/">
+              <Redirect to="/map" />
+            </Route>
+            <Route exact path="/map">
               <MapPage />
             </Route>
             <Route exact path="/fossil_gallery">
-              <FossilGalleryComponent />{" "}
+              <FossilGalleryComponent />
             </Route>
-            {/* <Route exact path="/" component={Main} /> */}
+            <Route path="/">
+              <div>No Match</div>
+            </Route>
           </Switch>
         </Suspense>
       </Container>
---
+++ src/components/Navbar.js
@@ -0,0 +1,85 @@
+import { Grid, makeStyles } from "@material-ui/core";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { skull_icon } from "../media";
+import { globe_icon } from "../media";
+
+const useStyles = makeStyles((theme) => ({
+  navLinkContainer: {},
+  navLink: {
+    // color: "#a52a2ab0",
+    color: "beige",
+    textDecoration: "none",
+    // padding: "20px 70px",
+    padding: "18px 65px",
+    fontWeight: 900,
+    position: "relative",
+    // backgroundImage: "linear-gradient(185deg, #f5f5dc70, #fedb00ba, #f5f5dcfc)",
+    // letterSpacing: 4,
+    // fontSize: 22,
+    borderRadius: "21px 21px 1px 1px",
+    textShadow: "-4px -1px 0px #434343",
+    backgroundColor: "#00000038",
+    "&:before": {
+      content: `url(${globe_icon})`,
+      top: 0,
+      left: 0,
+      position: "absolute",
+      filter: "drop-shadow(2px 4px 6px black)",
+    },
+  },
+  navLink2: {
+    color: "beige",
+    textDecoration: "none",
+    padding: "18px 65px",
+    fontWeight: 900,
+    position: "relative",
+    // backgroundImage: "linear-gradient(185deg, #f5f5dc70, #fedb00ba, #f5f5dcfc)",
+    // letterSpacing: 4,
+    // fontSize: 22,
+    borderRadius: "21px 21px 1px 1px",
+    textShadow: "-4px -1px 0px #434343",
+    backgroundColor: "#00000038",
+    "&:before": {
+      content: `url(${skull_icon})`,
+      top: 0,
+      left: 0,
+      position: "absolute",
+      filter: "drop-shadow(2px 4px 6px black)",
+    },
+  },
+  activeLink: {
+    borderBottom: "6px solid beige",
+    color: "beige",
+  },
+  imgIcon: {
+    width: 20,
+    height: 20,
+  },
+}));
+
+export const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <Grid container item xs={12} alignItems="center" justifyContent="center">
+      <Grid container item className={classes.navLinkContainer} xs={12} lg={4}>
+        <NavLink
+          to="/map"
+          className={classes.navLink}
+          activeClassName={classes.activeLink}
+        >
+          Prehistory Map
+        </NavLink>
+      </Grid>
+      <Grid container item className={classes.navLinkContainer} xs={12} lg={4}>
+        <NavLink
+          to="/fossil_gallery"
+          className={classes.navLink2}
+          activeClassName={classes.activeLink}
+        >
+          Human Fossil Gallery
+        </NavLink>
+      </Grid>
+    </Grid>
+  );
+};
