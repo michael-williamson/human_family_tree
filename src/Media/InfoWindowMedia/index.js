5a26d7f1b5205c37eb1c2049f161a6daa38cd255
michael-williamson <michaeldavid1721@yahoo.com>
2022-11-16 14:05:36 -0600

    updated imports

--- src/Media/InfoWindowMedia/index.js
+++ src/Media/InfoWindowMedia/index.js
@@ -1,5 +1,4 @@
 import savannah from "./savannah.jpg";
-import iceAgeTemperatures from "./Ice_Age_Temperature.png";
 import downRightArrow from "./down_right_arrow.png";
 
-export { savannah, iceAgeTemperatures, downRightArrow };
+export { savannah, downRightArrow };
