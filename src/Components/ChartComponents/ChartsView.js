4a0027f86c40306e6276d0aac6907d9a575b03fc
michael-williamson <michaeldavid1721@yahoo.com>
2023-04-26 13:38:08 -0500

    fixed broken component

--- src/Components/ChartComponents/ChartsView.js
+++ src/Components/ChartComponents/ChartsView.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Box } from "@mui/system";
-import { EventsTimeline } from "./IndividualCharts/EventsTimeline";
+// import { EventsTimeline } from "./IndividualCharts/EventsTimeline";
 // import { SpeciesTimeline } from "./IndividualCharts/SpeciesTimeline";
 import { SpecimensByContinent } from "./IndividualCharts/SpecimensByContinent";
 import { SpecimensBySpecies } from "./IndividualCharts/SpecimensBySpecies";
 
 export const ChartsView = (props) => {
   const chartsComponentObject = {
-    "Timeline of Events": <EventsTimeline />,
+    // "Timeline of Events": <EventsTimeline />,
     // "Timeline of Species": <SpeciesTimeline />,
     "Species by Continent": <SpecimensByContinent />,
     "Amount of Specimens by Species": <SpecimensBySpecies />,
