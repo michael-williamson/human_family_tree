6ad89f375173e531a5bf1f2257291fa858a6623a
michael-williamson <michaeldavid1721@yahoo.com>
2023-04-26 13:27:01 -0500

    added file to gitignore

--- src/Components/MapComponents/GoogleMapComponents/MapContainer.js
+++ src/Components/MapComponents/GoogleMapComponents/MapContainer.js
@@ -4,7 +4,6 @@ import { GoogleMapComponent } from "./GoogleMapComponent";
 import { mapContainerStyles } from "../../../Styles/MapComponentStyles/MapContainerStyles";
 import { MapLegendStateProvider } from "../MapStateComponents/MapLegendStateProvider";
 import { InfoWindowStateProvider } from "../MapStateComponents/InfoWindowStateProvider";
-import { MapLegendFieldsCount } from "../MapStateComponents/MapLegendFieldsCount";
 import { HTTPRequestStateProvider } from "../MapStateComponents/HTTPRequestStateProvider";
 import { MapPopulationStateContext } from "../MapStateComponents/MapPopulationStateContext";
 
@@ -14,11 +13,9 @@ export const MapContainer = () => {
       <MapPopulationStateContext>
         <HTTPRequestStateProvider>
           <MapLegendStateProvider>
-            <MapLegendFieldsCount>
-              <InfoWindowStateProvider>
-                <GoogleMapComponent />
-              </InfoWindowStateProvider>
-            </MapLegendFieldsCount>
+            <InfoWindowStateProvider>
+              <GoogleMapComponent />
+            </InfoWindowStateProvider>
           </MapLegendStateProvider>
         </HTTPRequestStateProvider>
       </MapPopulationStateContext>
