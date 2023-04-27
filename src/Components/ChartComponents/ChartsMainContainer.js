8d3fe6e2640dacb61c024553abc11026b61dea17
michael-williamson <michaeldavid1721@yahoo.com>
2023-04-27 08:13:51 -0500

    set up chart main container for controlling state

--- src/Components/ChartComponents/ChartsMainContainer.js
+++ src/Components/ChartComponents/ChartsMainContainer.js
@@ -1,23 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box } from "@mui/system";
-import { ChartsMenuContainer } from "./ChartsMenuContainer";
 import { ChartsView } from "./ChartsView";
-import { MapPopulationStateContext } from "../MapComponents/MapStateComponents/MapPopulationStateContext";
-import { HTTPRequestStateProvider } from "../MapComponents/MapStateComponents/HTTPRequestStateProvider";
+import { useSpecimensArrayContext } from "../MapComponents/MapStateComponents/MapPopulationStateContext";
+import { useNetworkRequestDispatch } from "../MapComponents/MapStateComponents/HTTPRequestStateProvider";
+import { MenuContainer } from "../ReusableComponents/MenuContainer";
+import {
+  menuButtonContainerStyles,
+  menuButtonStyles,
+  menuTitleStyles,
+} from "../../Styles/ReusableComponentStyles/MenuContainer.js";
+import { chartsArray } from "../../HelperFunctions/ChartComponent";
+import { SELECT_ALL, SPECIES } from "../../ConstantVariableNames";
 
 export const ChartsMainContainer = () => {
   const [chart, setChart] = useState();
+  const fetchArray = useNetworkRequestDispatch();
+  const specimensArray = useSpecimensArrayContext();
+  useEffect(() => {
+    fetchArray({
+      message: SELECT_ALL,
+      propertyName: SPECIES,
+    });
+    return () => {};
+  }, [fetchArray]);
   const handler = (item) => (e) => {
     setChart(item);
   };
   return (
     <Box>
-      <ChartsMenuContainer handler={handler} />
-      <MapPopulationStateContext>
-        <HTTPRequestStateProvider>
-          <ChartsView chart={chart} />
-        </HTTPRequestStateProvider>
-      </MapPopulationStateContext>
+      <MenuContainer
+        titleText="Choose a Chart:"
+        menuTitleStyles={menuTitleStyles}
+        menuButtonContainerStyles={menuButtonContainerStyles}
+        sourceArr={chartsArray}
+        clickHandler={handler}
+        menuButtonStyles={menuButtonStyles}
+      />
+      <ChartsView chart={chart} arr={specimensArray} />
     </Box>
   );
 };
