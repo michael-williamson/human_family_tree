9ebc4d5574e7f33187c8db08f219855bc355bdee
michael-williamson <michaeldavid1721@yahoo.com>
2023-06-14 12:40:32 -0500

    added new Provider components for checkbox state

---
+++ src/State/MapState/DatesCheckboxProvider.tsx
@@ -0,0 +1,37 @@
+import React, { useContext, useReducer } from "react";
+import { datesKeyObject } from "../../HelperFunctions/MapComponent/MapContainerComponent/StateMaintenanceFN";
+
+const initialState = datesKeyObject();
+
+const DatesCheckboxContext = React.createContext(initialState);
+const DatesCheckboxContextUpdater = React.createContext({});
+
+export function useDatesCheckbox() {
+  return useContext(DatesCheckboxContext);
+}
+
+export function useDatesCheckboxUpdater() {
+  return useContext(DatesCheckboxContextUpdater);
+}
+
+function checkboxReducer(state: any, { payload }: any) {
+  const { fieldName, bool } = payload;
+  return {
+    ...state,
+    [fieldName]: !bool,
+  };
+}
+
+export const DatesCheckboxProvider = ({ children }: any) => {
+  const [datesCheckboxState, datesCheckboxDispatch] = useReducer(
+    checkboxReducer,
+    initialState
+  );
+  return (
+    <DatesCheckboxContext.Provider value={datesCheckboxState}>
+      <DatesCheckboxContextUpdater.Provider value={datesCheckboxDispatch}>
+        {children}
+      </DatesCheckboxContextUpdater.Provider>
+    </DatesCheckboxContext.Provider>
+  );
+};
---
+++ src/State/MapState/OverlaysCheckboxProvider.tsx
@@ -0,0 +1,37 @@
+import React, { useContext, useReducer } from "react";
+import { overlaysKeyObject } from "../../HelperFunctions/MapComponent/MapContainerComponent/StateMaintenanceFN";
+
+const initialState = overlaysKeyObject();
+
+const OverlaysCheckboxContext = React.createContext(initialState);
+const OverlaysCheckboxContextUpdater = React.createContext({});
+
+export function useOverlaysCheckbox() {
+  return useContext(OverlaysCheckboxContext);
+}
+
+export function useOverlaysCheckboxUpdater() {
+  return useContext(OverlaysCheckboxContextUpdater);
+}
+
+function checkboxReducer(state: any, { payload }: any) {
+  const { fieldName, bool } = payload;
+  return {
+    ...state,
+    [fieldName]: !bool,
+  };
+}
+
+export const OverlaysCheckboxProvider = ({ children }: any) => {
+  const [overlaysCheckboxState, overlaysCheckboxDispatch] = useReducer(
+    checkboxReducer,
+    initialState
+  );
+  return (
+    <OverlaysCheckboxContext.Provider value={overlaysCheckboxState}>
+      <OverlaysCheckboxContextUpdater.Provider value={overlaysCheckboxDispatch}>
+        {children}
+      </OverlaysCheckboxContextUpdater.Provider>
+    </OverlaysCheckboxContext.Provider>
+  );
+};
