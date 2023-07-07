3e08eea3a37d11d974cf3313b4b7432f5a11ffac
michael-williamson <michaeldavid1721@yahoo.com>
2023-07-07 11:34:02 -0500

    converted App.js to TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
-
 import reportWebVitals from "./reportWebVitals";
+import App from "./App";
 
 if (typeof window !== "undefined") {
   window.React = React;
